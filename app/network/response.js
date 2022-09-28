e3bfab0616bb544883b26a959f46f94459a34c0e
carloscastillo10 <carloscastillo090916@gmail.com>
2022-09-28 15:03:10 -0500

    :hammer: Update status code and status message

--- app/network/response.js
+++ app/network/response.js
@@ -1,15 +1,37 @@
+const statusMessages = {
+    200: 'OK',
+    201: 'Created',
+    400: 'Invalid data',
+    500: 'Internal error',
+};
 function success(req, res, message, status) {
-    res.status(status || 200).send({
+    let statusCode = status;
+    let statusMessage = message;
+    if (!status) {
+        statusCode = 200;
+    }
+    if (!message) {
+        statusMessage = statusMessages[statusCode];
+    }
+    res.status(statusCode).send({
         error: '',
-        body: message
-    })
+        body: statusMessage,
+    });
 }
 
 function error(req, res, message, status) {
-    res.status(status || 500).send({
-        error: message,
-        body: ''
-    })
+    let statusCode = status;
+    let statusMessage = message;
+    if (!status) {
+        statusCode = 500;
+    }
+    if (!message) {
+        statusMessage = statusMessages[statusCode];
+    }
+    res.status(statusCode).send({
+        error: statusMessage,
+        body: '',
+    });
 }
 
 module.exports = { success, error };
