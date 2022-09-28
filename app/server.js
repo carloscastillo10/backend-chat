6e639f22e5a9e3f1c3060c53f1cfe4fc0ed7dcdb
carloscastillo10 <carloscastillo090916@gmail.com>
2022-09-28 10:14:57 -0500

    :zap: Refactor database connection

--- app/server.js
+++ app/server.js
@@ -4,16 +4,14 @@ const routerApi = require('./network/routes');
 const { config } = require('./config/config');
 const connectDatabase = require('./db/database');
 
-const port = process.env.PORT || 3000;
 var app = express();
+connectDatabase(config.dbUrl);
 
 app.use(bodyParser.json());
-
-connectDatabase(config.dbUrl);
 routerApi(app);
 
 app.use('/app', express.static('public')); // Serve static files
 
-app.listen(port, () => {
-    console.log('My port ' + port);
+app.listen(config.port, () => {
+    console.log('My port ' + config.port);
 });
